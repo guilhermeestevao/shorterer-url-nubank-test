b893265959967d780d94e99e335dfb468aa6e4f2
Guilherme <guilherme@MacBook-Pro-de-Guilherme.local>
2023-10-21 14:59:13 -0300

    Update sdk compiles on modules

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "test.example.data"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 26
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "test.example.presentation"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 26
